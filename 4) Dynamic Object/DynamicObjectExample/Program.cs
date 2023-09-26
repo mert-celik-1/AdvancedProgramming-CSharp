c9a82ebca72c31b9f2ede905ab6cdc5a084c4d05
mert-celik-1 <mertcelik2399@gmail.com>
2023-09-26 17:40:09 +0300

    dynamic object

---
+++ 4) Dynamic Object/DynamicObjectExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Dynamic;
+
+#region  Activator Sınıfı ile Nesne Oluşturma
+MyClass m = (MyClass)Activator.CreateInstance(typeof(MyClass));
+#endregion
+
+#region DynamicObject ile Nesne Oluşturma
+
+dynamic instance = new MyClass();
+instance.prop1 = 123;
+instance.prop2 = "124";
+
+#endregion
+
+class MyClass : DynamicObject
+{
+	public MyClass()
+	{
+		Console.WriteLine($"{nameof(MyClass)} instance created");
+	}
+
+    readonly private Dictionary<string, object> properties = new();
+
+    public override bool TrySetMember(SetMemberBinder binder, object? value)
+    {
+        properties.Add(binder.Name, value);
+        return true;
+    }
+
+    public override bool TryGetMember(GetMemberBinder binder, out object? result)
+    {
+        return properties.TryGetValue(binder.Name, out result); 
+    }
+
+}
