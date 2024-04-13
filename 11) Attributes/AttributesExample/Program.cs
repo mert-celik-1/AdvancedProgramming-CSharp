b256b8cd0d50261932bfa7b58cc5d28ddf780fae
mert-celik-1 <mertcelik2399@gmail.com>
2024-04-13 22:17:43 +0300

    attributes

---
+++ 11) Attributes/AttributesExample/Program.cs
@@ -0,0 +1,19 @@
+﻿// Yazılım elemanlarının davranışına yapılandırmasına veya görünümüne müdahele etmek için kullanılan bir özelliktir.
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+class MyAttribute : Attribute
+{
+    //Attributelerde sadece propertylere erişim vardır. Bu ctor içerisinde yada düz tanımlanmış property olabilir
+    public MyAttribute(int a)
+    {
+        
+    }
+
+    public int b { get; set; }
+    
+}
+
+[My(5,b=20)]
+class MyClass
+{
+    
+}
