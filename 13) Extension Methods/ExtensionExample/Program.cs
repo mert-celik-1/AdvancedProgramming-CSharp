4d88fc3abe31d3e6acc9d333c9562b278f6cf784
mert-celik-1 <mertcelik2399@gmail.com>
2024-04-15 16:15:41 +0300

    Extension

---
+++ 13) Extension Methods/ExtensionExample/Program.cs
@@ -0,0 +1,25 @@
+﻿// Class ve struct yapılarını modify etmeden ilgili struct yada class'için extension metodlar eklememizi sağlar.
+
+/*Extesion metod yazarken uymamız gereken bir kaç kural vardır. Bunlar;
+
+Extension metodlar static bir class içerisinde static olarak tanımlanmalıdır. 
+    Extend edilecek class ilgili extension metoda metodun ilk parametresi olarak verilip önünde this keyword'ü ile tanımlanmalıdır
+Extension metod da tanımlı parametrelerden sadece 1 tanesi this keyword'ü ile tanımlanır */
+
+Console.WriteLine("Hello, World!");
+
+MyClass m = new();
+m.X();
+
+class MyClass
+{
+    
+}
+
+static class ExtensionMethods
+{
+    public static void X(this MyClass i)
+    {
+        
+    }
+}
